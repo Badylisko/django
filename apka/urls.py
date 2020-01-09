5531ca86e0aa16ffa2d2b5cea1e64dcb34e1f220
Badylisko <saejs12@gmail.com>
2020-01-09 21:27:22 +0100

    Dalej

--- apka/urls.py
+++ apka/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, kontakt
+from .views import kontakt
+from .views import PostListView, PostDetailView
 
 
 urlpatterns = [
-    path('', home, name='Home'),
+    path('', PostListView.as_view(), name='Home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('kontakt/', kontakt, name='Kontakt')
 ]

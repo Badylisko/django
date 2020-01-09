8aca3eb476a69d4780c5669431f7cb88891392d7
Badylisko <saejs12@gmail.com>
2020-01-10 00:39:46 +0100

    Update 5

--- apka/urls.py
+++ apka/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import kontakt
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserListView
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='Home'),
+    path('user/<str:username>', UserListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
